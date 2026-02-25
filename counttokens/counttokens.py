61310678e6ff9a0ab473b1e4793c02bd1e4882e7
Linus Fernandes <linus.fernandes@gmail.com>
2026-02-25 15:59:07 +0530

    feat: continue with count tokens

---
+++ counttokens/counttokens.py
@@ -0,0 +1,40 @@
+"""
+Counttokens.
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : counttokens
+# @created     : Wednesday Feb 25, 2026 15:13:22 IST
+# @description :
+# -*- coding: utf-8 -*-'
+######################################################################
+"""
+import tiktoken
+
+token_string = "tiktoken is great!"
+encoding = tiktoken.get_encoding("cl100k_base")
+
+print(encoding)
+
+print(f"Encoding '{token_string}'")
+
+encoded = encoding.encode(token_string)
+
+print(encoded)
+encoding = tiktoken.encoding_for_model("gpt-4o-mini")
+
+print(encoding)
+
+print(f"Encoding '{token_string}'")
+encoded = encoding.encode(token_string)
+print(encoded)
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+encoding_name = "o200k_base"
+print(f"Getting number of tokens for string '{token_string}' using encoding '{encoding_name}'")
+num_tokens_from_string(token_string, encoding_name)
