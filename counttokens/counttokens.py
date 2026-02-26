cff1a35c82c2ffe62ea6a07b8fc5e1dbedf3d1f6
Linus Fernandes <linus.fernandes@gmail.com>
2026-02-26 13:54:37 +0530

    feat: complete count tokens example as https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb

--- counttokens/counttokens.py
+++ counttokens/counttokens.py
@@ -169,3 +169,135 @@ for model in [
     if response.usage is not None:
         print(f'{response.usage.prompt_tokens} prompt tokens counted by the OpenAI API.')
     print()
+
+def num_tokens_for_tools(functions, messages, model):
+
+    # Initialize function settings to 0
+    func_init = 0
+    prop_init = 0
+    prop_key = 0
+    enum_init = 0
+    enum_item = 0
+    func_end = 0
+
+    if model in [
+        "gpt-4o",
+        "gpt-4o-mini"
+    ]:
+
+        # Set function settings for the above models
+        func_init = 7
+        prop_init = 3
+        prop_key = 3
+        enum_init = -3
+        enum_item = 3
+        func_end = 12
+    elif model in [
+        "gpt-3.5-turbo",
+        "gpt-4"
+    ]:
+        # Set function settings for the above models
+        func_init = 10
+        prop_init = 3
+        prop_key = 3
+        enum_init = -3
+        enum_item = 3
+        func_end = 12
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_for_tools() is not implemented for model {model}."""
+        )
+
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using o200k_base encoding.")
+        encoding = tiktoken.get_encoding("o200k_base")
+
+    func_token_count = 0
+    if len(functions) > 0:
+        for f in functions:
+            func_token_count += func_init  # Add tokens for start of each function
+            function = f["function"]
+            f_name = function["name"]
+            f_desc = function["description"]
+            if f_desc.endswith("."):
+                f_desc = f_desc[:-1]
+            line = f_name + ":" + f_desc
+            func_token_count += len(encoding.encode(line))  # Add tokens for set name and description
+            if len(function["parameters"]["properties"]) > 0:
+                func_token_count += prop_init  # Add tokens for start of each property
+                for key in list(function["parameters"]["properties"].keys()):
+                    func_token_count += prop_key  # Add tokens for each set property
+                    p_name = key
+                    p_type = function["parameters"]["properties"][key]["type"]
+                    p_desc = function["parameters"]["properties"][key]["description"]
+                    if "enum" in function["parameters"]["properties"][key].keys():
+                        func_token_count += enum_init  # Add tokens if property has enum list
+                        for item in function["parameters"]["properties"][key]["enum"]:
+                            func_token_count += enum_item
+                            func_token_count += len(encoding.encode(item))
+                    if p_desc.endswith("."):
+                        p_desc = p_desc[:-1]
+                    line = f"{p_name}:{p_type}:{p_desc}"
+                    func_token_count += len(encoding.encode(line))
+        func_token_count += func_end
+
+    messages_token_count = num_tokens_from_messages(messages, model)
+    total_tokens = messages_token_count + func_token_count
+
+    return total_tokens
+
+from openai.types.chat import ChatCompletionToolParam
+
+tools:list[ChatCompletionToolParam] = [
+  {
+    "type": "function",
+    "function": {
+      "name": "get_current_weather",
+      "description": "Get the current weather in a given location",
+      "parameters": {
+        "type": "object",
+        "properties": {
+          "location": {
+            "type": "string",
+            "description": "The city and state, e.g. San Francisco, CA",
+          },
+          "unit": {"type": "string",
+                   "description": "The unit of temperature to return",
+                   "enum": ["celsius", "fahrenheit"]},
+        },
+        "required": ["location"],
+      },
+    }
+  }
+]
+
+example_messages:list[ChatCompletionMessageParam] = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant that can answer to questions about the weather.",
+    },
+    {
+        "role": "user",
+        "content": "What's the weather like in San Francisco?",
+    },
+]
+
+for model in [
+    "gpt-3.5-turbo",
+    "gpt-4",
+    "gpt-4o",
+    "gpt-4o-mini"
+    ]:
+    print(model)
+    # example token count from the function defined above
+    print(f"{num_tokens_for_tools(tools, example_messages, model)} prompt tokens counted by num_tokens_for_tools().")
+    # example token count from the OpenAI API
+    response = client.chat.completions.create(model=model,
+          messages=example_messages,
+          tools=tools,
+          temperature=0)
+    assert response.usage is not None
+    print(f'{response.usage.prompt_tokens} prompt tokens counted by the OpenAI API.')
+    print()
